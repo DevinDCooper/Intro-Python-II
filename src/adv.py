2753f4ae1fa836a3110c0dca6ba888f13a6dbf56
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-07-17 14:55:44 -0700

    mvp_day_2_one_bug_

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,26 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",Item('bow')),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""",Item('sword')),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", Item("water")),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", Item("shield")),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", Item('gold')),
 }
 
 
@@ -56,14 +57,28 @@ print(player)
 # If the user enters "q", quit the game.
 while True:
     current_room = player.current_room
+    player_inventory = [x.name for x in player.inventory] if len(player.inventory) else "Empty Bag"
+    room_inventory = [ x.name for x in current_room.item] if len( current_room.item) else "Empty Room"
+
+
+
     print(f'You are located at {current_room.name} and {current_room.description}')
-    print("Choose next path.")
+    print(f"You have found a {room_inventory}\n")
+    print(f"If you would like to take an item, type: take item_name\n")
+    print(f"If you would like to drop an item, type: drop item_name\n")
+
+    print(f'{player_name} has {len(player.inventory)} item(s) in there bag\n')
+    print(f'Bag: {player.inventory}')
+    print("Choose next path.\n")
+
     enter = input('Enter n, s , e, w and q to quit the game: ')
+
+    action = enter[0:4]
+    item = enter[5: ]
 
     if enter == 'q':
             print(f'Thanks for playing, {player_name}. Till next time!')
     if enter == 'n':
-        # The hasattr() method returns true if an object has the given named attribute and false if it does not.
         if hasattr(current_room, 'n_to'):
             print("going North")
             player.current_room = current_room.n_to
@@ -86,6 +101,20 @@ while True:
             print(f'{player_name} going out West')
             player.current_room = current_room.w_to
             print(f'{player_name} enter another location, you hit a invisible wall ')
+
+    elif action == "drop":
+        item_name_list = [i.name for i in player_inventory]
+        item_index = item_name_list.index(item)
+        current_room.add_item(player.inventory[item_index])
+        player.drop(item_index)
+
+    elif action == "take" :
+        item_name_list = [i.name for i in current_room.item]
+        item_index = item_name_list.index(item)
+        player.take(current_room.item[item_index])
+        current_room.delete_item(item_index)
+
+
     else:
             print('Please enter a valid location')
             break
--- src/room.py
+++ src/room.py
@@ -1,9 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from item import Item
+
 class Room:
-        def __init__(self, name, description):
+        def __init__(self, name, description, item):
             self.name = name
             self.description = description
+            self.item = [item]
 
         def __str__(self):
-            return f'{self.name} {self.description}'
+            return f'Room : {self.name}, description : {self.description}'
+
+        def add_item(self, item):
+            self.item.append(item)
+
+        def delete_item(self, item):
+            del self.item[item]
